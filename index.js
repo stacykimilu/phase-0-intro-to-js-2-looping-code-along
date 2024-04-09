a6acd138f915703db44fc8901afba0e7c42eef67
Stacy <stacykimilu@gmail.com>
2024-04-09 10:00:53 +0300

    finished looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names) {
+  let thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    thankYouMessages.push(message); // Changed from messages to thankYouMessages
+  }
+  return thankYouMessages; // Changed from messages to thankYouMessages
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+countDown(10);
+
